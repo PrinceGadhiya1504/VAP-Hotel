6df604438323aa09d578c9158fe3fcaed9407dcf
PrinceGadhiya1504 <ppgadhiya2003@gmail.com>
2024-08-05 15:57:11 +0530

    Payment Schema change

--- server/model/Payment.js
+++ server/model/Payment.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+
 const paymentSchema = new mongoose.Schema({
     bookingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
@@ -7,12 +8,10 @@ const paymentSchema = new mongoose.Schema({
     bank: { type: String, required: true },
     paymentMethod: { type: String, enum: ['creditCard', 'debitCard', 'upi', 'wallet'], required: true },
     paymentStatus: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
-    paymentDate: { type: Date, required: true, default: Date.now },
-    transactionId: { type: String, unique: true },
-    // currency: { type: String, default: 'INR' },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
-})
+    paymentDate: { type: Date, default: Date.now },
+    transactionId: { type: String, unique: true, required: true },
+    currency: { type: String, default: 'INR' },
+  }, { timestamps: true });
 
 const Payment = mongoose.model('Payment', paymentSchema)
 module.exports = Payment
